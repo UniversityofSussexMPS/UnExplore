fb2b60ce01094fdf69f3357a415cd94fda702f4a
ECallanan <erincallanan1996@gmail.com>
2017-08-16 12:03:11 +0100

    reset

--- models/solarsystem.js
+++ models/solarsystem.js
@@ -1,4 +1,3 @@
-
 var WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight;
 
@@ -170,10 +169,7 @@ function add_stars(){
     //geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
     geometry.computeBoundingSphere();
     //
-    //set up colors for stars
-    var  starColors = ['#9db4ff', '#a2b9ff','#a7bcff','#aabfff','#afc3ff','#baccff','#c0d1ff','#cad8ff','#e4e8ff','#edeeff','#fbf8ff','#fff9f9','#fff5ec','#fff4e8','#fff1df','#ffebd1','#ffd7ae','#ffc690','#ffbe7f','#ffbb7b'];
-    //
-    var material = new THREE.PointsMaterial( { color: starColors[Math.floor(Math.random()*starColors.length)], size: 10, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true } );
+    var material = new THREE.PointsMaterial( { size: 10, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true } );
     Stars2D = new THREE.Points( geometry, material );
     scene.add( Stars2D );
 
@@ -273,7 +269,6 @@ function render_all() {
 
 function create_3Dstars(){
     /*
-
     var i, r = 1000,
         starsGeometry = [new THREE.Geometry(), new THREE.Geometry()];
     for (i = 0; i < 250; i++) {
@@ -323,9 +318,8 @@ function create_3Dstars(){
     //geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
     geometry.computeBoundingSphere();
     //
-    var material = new THREE.PointsMaterial( { color: starColors[Math.floor(Math.random()*starColors.length)] ,size: 5000, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true } );
+    var material = new THREE.PointsMaterial( { size: 5000, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true } );
     Stars3D = new THREE.Points( geometry, material );
     Stars3D.scale.set(2e6, 2e6, 2e6);
     Stars3D.updateMatrix();
 };
-
