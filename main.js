3005bd6984e4789fef6bcaa717395c72afec3949
ECallanan <erincallanan1996@gmail.com>
2017-08-17 15:15:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -125,7 +125,6 @@ function init() {
 	
 	scene.add(proton1,proton2,neutron1,neutron2);
 	scene.add(particleSystem);
-	scene.add(addElectron)
 	simElectron = true;
 	message.innerHTML="This is an atomic nucleus surrounded by an electron cloud";
 
