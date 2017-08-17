dacd6be7629af1c198dba7189f9c6b010d5caf12
ECallanan <erincallanan1996@gmail.com>
2017-08-17 11:59:14 +0100

    add light

--- models/CMB.js
+++ models/CMB.js
@@ -4,5 +4,6 @@ function createCMB(){
 	var geometry = new THREE.SphereBufferGeometry( 30, 32, 32 );
 	var material = new THREE.MeshBasicMaterial( {map:CMBmap} );
 	CMBsphere = new THREE.Mesh( geometry, material );
+	scene.add(light, ambientLight);
 	
 };
