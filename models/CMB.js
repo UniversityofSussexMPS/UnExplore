83c7f52c78578aa3bfd42743c731b119f6d10190
ECallanan <erincallanan1996@gmail.com>
2017-08-17 12:54:05 +0100

    Update CMB.js

--- models/CMB.js
+++ models/CMB.js
@@ -4,6 +4,5 @@ function createCMB(){
 	var geometry = new THREE.SphereBufferGeometry( 30, 32, 32 );
 	var material = new THREE.MeshBasicMaterial( {map:CMBmap} );
 	CMBsphere = new THREE.Mesh( geometry, material );
-	scene.add(light, ambientLight);
 	
 };
