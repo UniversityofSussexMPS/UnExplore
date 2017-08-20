91fb53a211d7e5b27e561dc385e66a4dd2091ec0
ECallanan <erincallanan1996@gmail.com>
2017-08-20 19:01:45 +0100

    Update CMB.js

--- models/CMB.js
+++ models/CMB.js
@@ -1,5 +1,5 @@
 function createCMB(){
-	CMBmap= textureLoader.load("../images/CMB.jpg")
+	CMBmap= textureLoader.load("images/CMB.jpg")
 
 	var geometry = new THREE.SphereBufferGeometry( 30, 32, 32 );
 	var material = new THREE.MeshBasicMaterial( {map:CMBmap} );
