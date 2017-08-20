547f4dc812aa69a91f987da053ac3d8420d06e70
ECallanan <erincallanan1996@gmail.com>
2017-08-20 21:52:32 +0100

    Update GAMA_pos.js

--- models/GAMA_pos.js
+++ models/GAMA_pos.js
@@ -8,11 +8,9 @@ function addGamaData(){
 		  
 	GAMA_geometry.computeBoundingSphere();
 	
-	//make random star colour function
-	var starColors = ['#9db4ff', '#a2b9ff','#a7bcff','#aabfff','#afc3ff','#baccff','#c0d1ff','#cad8ff','#e4e8ff','#edeeff','#fbf8ff','#fff9f9','#fff5ec','#fff4e8','#fff1df','#ffebd1','#ffd7ae','#ffc690','#ffbe7f','#ffbb7b'];
 		  
 		
-	var material = new THREE.PointsMaterial( { color: starColors[Math.floor(Math.random()*starColors.length)], size: 1.5, sizeAttenuation: true,map: sprite, alphaTest: 0.1, transparent:false } );
+	var material = new THREE.PointsMaterial( { size: 1.5, sizeAttenuation: true,map: sprite, alphaTest: 0.1, transparent:false } );
 		
 	GAMA_Z = new THREE.Points( GAMA_geometry, material );
 	GAMA_Z.rotation.x=Math.PI/2
