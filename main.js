8592764daaf96f24b7ab95f4e2c0a83816c45456
ECallanan <erincallanan1996@gmail.com>
2017-08-22 11:56:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -60,7 +60,206 @@ $( "#slider-vertical" ).slider({
 	}
 
 });
+var controls, reCenterTimeout, typeface="http://cdn.rawgit.com/ECallanan/hello-world/c2eb3129/Abscissa_Bold.js";
+			
+			var winWidth = window.innerWidth;
+			var WinHeight = window.innerHeight;
+			
+			//default text values
+			var defaults = {
+				h: 8,		//height
+				s: 20,		//size
+				cS: 4,		//curve segments
+				bT: 1,		//bevel thickness
+				bSz: 1.5,	//bevel size
+				bSg: 3,		//bevel segments
+				bE: true,	//bevel enabled
+				c: grey,	//colour
+				y: -64,
+				z: -1
+			}
+			
+			//scene object
+			var scene = {
+				backgroundColor: black;
+				cameraX: 0,
+				cameraY: 0,
+				cameraZ: 950,
+				pointLightIntensity: 1
+			}
+			
+			//text objects
+			var title = {
+				t: "unExplore",		//title
+				h: 20,				//height
+				s: 60,				//size
+				x: "centered",
+				y: -27,
+				z: -14
+			}
+			
+			//array with all text
+			var texts = [title];
+			
+			
+			function loadAllText(texts) {
+				for(t = 0; t < texts.length; t++) {
+					loadFont(texts[t]);
+				}
+			}
+			
+			function loadFont(texts) {
+				var fontLoader = new THREE.FontLoader();
+				fontLoader.load(typeface, function( response) {
+					text.f = response;
+					refreshText(text);
+				});
+			}
+			
+			function createText(text) {
+				var f = text.f,
+					s = text.s || defaults.s,
+					h = text.h || defaults.h,
+					cSg = text.cSg || defaults.cSg,
+					bT = text.bT || defaults.bT
+					bSz = text.bSz || defaults.bSz,
+					bSg = text.bSg || defaults.bSg,
+					bE = text.bE || defaults.bE,
+					c = text.c || defaults.c,
+					y = text.y || defaults.y,
+					z = text.z || defaults.z;
+					
+				text.geometry= new THREE.TextGeometry(text.t, {
+				
+					font: f,
+					height: h,
+					size: s,
+					curveSegments: cSg,
+					bevelThickness: bT,
+					bevelsize: bSz,
+					bevelSegments: bSg,
+					bevelEnabled: bE,
+					material: 0,
+					extrudeMaterial: 1
+					
+				});
+				
+				text.material = new THREE.MultiMaterial( [
+					new THREE.MeshPhongMaterial({color: c, shading: THREE.FlatShading}),//front
+					new THREE.MeshPhongMaterial({color: c, shading: THREE.SmoothShading})//side
+				]);
+				
+				text.geometry.computeBoundingBox();
+				text.geometry.computeVertexNormals();
+				
+				text.centerOffset = -0.5*(text.geometry.boundingBox.max.x - text.geometry.boundingBox.min.x);
+				
+				if(text.x === "centered")text.x = text.centerOffset;
+				
+				text.mesh.position.set(text.x, y, z);
+				
+				//add text to scene, combine all
+				scene.scene.add(text.mesh);
+			}
+			
+			function refreshText(text) {
+				scene.scene.remove(text.mesh);
+				createText(text);
+			}
+			
+			function render() {
+				camera.lookAt(cameraTarget);
+				renderer.clear();
+				renderer.render(scene.scene, camera);
+			}
+			
+			//tweening
+			
+			function bringInText() {
+				var introTimeline = new TimelineMax({
+					paused: true,
+					delay: .5
+				});
+				introTimeline
+				
+					.to(pointLight, 1, {
+						intensity: 2,
+						ease: Power2.easeIn
+					}, "1")
+					
+					.to(camera.position, 2, {
+						x: 0,
+						y: 0,
+						z: 650
+					}, "0" )
+					
+				.play();
+			}
 
+
+init();
+animate();
+
+function initTitle() {
+	/*
+		function to initilize the three js scene
+	*/
+
+	// Initialize the scene
+	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0x000000, 0.0000000025 );
+
+	// Setup renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor( scene.fog.color );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	// Creates containeer to show the framerate
+	var container = document.getElementById( 'container' );
+	container.appendChild( renderer.domElement );
+
+	// Create the Camera
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 1e13 );
+	camera.position.set(0,0,0.02)
+
+
+	// Set the control
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.enableZoom = false;
+	controls.enablePan = false;
+
+
+	//Create light
+	light = new THREE.DirectionalLight( 0xffffff,1 );
+	light.position.set( 0, 0, 10 );
+	light.target.position.set(0,0,0);
+	light.castShadow = true;
+	ambientLight = new THREE.AmbientLight(0xffffff);
+	
+	// Create models
+	Creat
+	
+	// add the starts
+	stats = new Stats();
+	container.appendChild( stats.dom );
+
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+	// load in the gama data in the background so the website takes less time to load
+	loadScript("data/GAMA_data.js",addGamaData)
+
+	//Add the first model to the scene
+	scene.add(proton1, proton2,neutron1,neutron2);
+	scene.add(particleSystem);
+	simElectron = true;
+	message.innerHTML="This is a Helium nucleus surrounded by a cloud of electrons";
+	
+
+	//Initalize all the tweens
+	initTweens();
+	
 
 init();
 animate();
