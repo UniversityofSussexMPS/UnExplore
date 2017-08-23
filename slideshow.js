129ca869e8027add66db068357081238ef342034
ECallanan <erincallanan1996@gmail.com>
2017-08-23 16:17:37 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,8 +1,14 @@
- $("#unexplore-slideshow > div:gt(0)").hide();setInterval(function() {
-    $("#unexplore-slideshow > div:first")
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo("#unexplore-slideshow");
-    }, 3000);
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
