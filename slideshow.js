ab543025eae264d32b231f67edb937c54c35774e
ECallanan <erincallanan1996@gmail.com>
2017-08-23 16:20:26 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,7 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("slides");
+    var x = document.getElementsByClassName("slide");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none"; 
     }
