8a7fb627b371efa897c7f6af7c31215ce97707e3
ECallanan <erincallanan1996@gmail.com>
2017-08-24 15:22:52 +0100

    Update unexplore.js

--- models/unexplore.js
+++ models/unexplore.js
@@ -1,79 +1,66 @@
+var typeface="http://cdn.rawgit.com/ECallanan/hello-world/c2eb3129/Abscissa_Bold.js";
 
-			var reCenterTimeout, typeface="http://cdn.rawgit.com/ECallanan/hello-world/c2eb3129/Abscissa_Bold.js";
-			
-			var winWidth = window.innerWidth;
-			var WinHeight = window.innerHeight;
-	
-			
-			//text objects
-			var title = "unExplore",		//title
-				height= 20,			
-				size= 60,
-				cS= 4,				//curve segments
-				bT= 1,		//bevel thickness
-				bSz= 1.5,	//bevel size
-				bSg= 3,		//bevel segments
-				bE= true,	//bevel enabled
-				c= grey,	//colour
-			    	font= undefined,
-			    	fontName= typeface,
-				x= "centered",
-				y= -27,
-				z= -14
-			}
-			
-			
-			function loadAllText(texts) {
-				for(t = 0; t < texts.length; t++) {
-					loadFont(texts[t]);
+var  textMesh, textGeo, materials;
+var firstLetter = true;
+var text = "unExplore",
+	height = 20,
+	size = 70,
+	hover = 30,
+	curveSegments = 4,
+	bevelThickness = 2,
+	bevelSize = 1.5,
+	bevelSegments = 3,
+	bevelEnabled = true,
+	font = typeface,
+	fontWeight = "bold"; 
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+materials = [
+	new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
+	new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
+];
+function createText() {
+	textGeo = new THREE.TextBufferGeometry( text, {
+		font: font,
+		size: size,
+		height: height,
+		curveSegments: curveSegments,
+		bevelThickness: bevelThickness,
+		bevelSize: bevelSize,
+		bevelEnabled: bevelEnabled,
+		material: 0,
+		extrudeMaterial: 1
+	});
+	textGeo.computeBoundingBox();
+	textGeo.computeVertexNormals();
+	if ( ! bevelEnabled ) {
+		var triangleAreaHeuristics = 0.1 * ( height * size );
+		for ( var i = 0; i < textGeo.faces.length; i ++ ) {
+			var face = textGeo.faces[ i ];
+			if ( face.materialIndex == 1 ) {
+				for ( var j = 0; j < face.vertexNormals.length; j ++ ) {
+					face.vertexNormals[ j ].z = 0;
+					face.vertexNormals[ j ].normalize();
+				}
+				var va = textGeo.vertices[ face.a ];
+				var vb = textGeo.vertices[ face.b ];
+				var vc = textGeo.vertices[ face.c ];
+				var s = THREE.GeometryUtils.triangleArea( va, vb, vc );
+				if ( s > triangleAreaHeuristics ) {
+					for ( var j = 0; j < face.vertexNormals.length; j ++ ) {
+						face.vertexNormals[ j ].copy( face.normal );
+					}
 				}
 			}
-			
-			function loadFont(texts) {
-				var fontLoader = new THREE.FontLoader();
-				fontLoader.load(typeface, function( response) {
-					text.f = response;
-					refreshText(text);
-				});
-			}
-			
-			function createText() {
-				geometry= new THREE.TextGeometry(title, {
-				
-					fontName,
-					height,
-					size: s,
-					curveSegments: cSg,
-					bevelThickness: bT,
-					bevelsize: bSz,
-					bevelSegments: bSg,
-					bevelEnabled: bE,
-					material: 0,
-					extrudeMaterial: 1
-					
-				});
-				
-				material = new THREE.MultiMaterial( [
-					new THREE.MeshPhongMaterial({color: c, shading: THREE.FlatShading}),//front
-					new THREE.MeshPhongMaterial({color: c, shading: THREE.SmoothShading})//side
-				]);
-				
-				geometry.computeBoundingBox();
-				geometry.computeVertexNormals();
-				
-				centerOffset = -0.5*(geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-				
+		}
+	}
+	var centerOffset = -0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+	textMesh = new THREE.Mesh( textGeo, materials );
+	textMesh.position.x = centerOffset;
+	textMesh.position.y = hover;
+	textMesh.position.z = 0;
+	textMesh.rotation.x = 0;
+	textMesh.rotation.y = Math.PI * 2;
 	
-			};
-			Title = new THREE.Mesh(material, geometry)
-			if(x === "centered")x = centerOffset;
-				Title.position.set(x, y, z);
-			
-			function refreshText() {
-				scene.scene.remove(Title);
-				createText();
-			}
-			
-			
-		
-		
