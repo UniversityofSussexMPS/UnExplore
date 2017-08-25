e6a22b6918867fca505fda7a7c3bb224e5e1ae06
ECallanan <erincallanan1996@gmail.com>
2017-08-25 16:40:08 +0100

    Update unexplore.js

--- models/unexplore.js
+++ models/unexplore.js
@@ -1,28 +1,8 @@
 
-var titleMesh;
-
-function addTitle() {
-	
-	var fontLoader new THREE.FontLoader();
-	var font = fontLoader.load("fonts/Abscissa_Bold.js");
-	//get text from hash
-	var theTitle = "unExplore";
-	var hash = document.location.hash.substr(1);
-	if (hash.length !== 0) {
-		theTitle = hash;
-	}
-	var geometry = new THREE.TextGeometry(theTitle, {
-		font: font,
-		size: 80,
-		height: 20,
-		curveSegments: 2
-	});
-	var materials = [
-		new THREE.MeshPhongMaterial({color: 0x8c1717} ), //front
-		new THREE.MeshPhongMaterial({color: 0x8c1717} )//side
-	];
-	
-	titleMesh = new THREE.Mesh(geometry, materials);
-	titleMesh.position.set = (0,0,0)
-}
-
+var material = new THREE.MeshPhongMaterial({
+	color: 0x8c1717
+});
+var textGeom = new THREE.TextGeometry('unExplore', {
+	font: 'abscissa_bold'
+});
+var titleMesh = new THREE.Mesh(textGeom, material);
