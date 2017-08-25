33f92dae94f95c9340a3d73ad80c044ade37a396
ECallanan <erincallanan1996@gmail.com>
2017-08-25 17:11:36 +0100

    Update unexplore.js

--- models/unexplore.js
+++ models/unexplore.js
@@ -1,10 +1,7 @@
-function createTitle(){
-	
-	var material = new THREE.MeshPhongMaterial({
-		color: 0x8c1717
-	});
-	var textGeom = new THREE.TextGeometry('unExplore', {
-		font: 'abscissa_bold'
-	});
-	var titleMesh = new THREE.Mesh(textGeom, material);
-}
+var material = new THREE.MeshPhongMaterial({
+	color: 0x8c1717
+});
+var textGeom = new THREE.TextGeometry('unExplore', {
+	font: 'abscissa_bold'
+});
+var titleMesh = new THREE.Mesh(textGeom, material);
