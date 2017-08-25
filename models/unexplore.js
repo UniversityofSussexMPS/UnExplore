1461b48b037eeb1bc01773c2f88d4b7553bf9a9a
ECallanan <erincallanan1996@gmail.com>
2017-08-25 15:22:08 +0100

    Update unexplore.js

--- models/unexplore.js
+++ models/unexplore.js
@@ -1,13 +1,6 @@
 
-var group;
-var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
+var titleMesh, group;
 
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
-
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
 
 var loader = new THREE.Fontloader();
 loader.load('fonts/Abscissa_Bold.js', function(font) {
@@ -36,62 +29,12 @@ function initText(font) {
 		new THREE.MeshPhongMaterial({color: 0x8c1717, overdraw: 0.5} )
 	];
 	
-	
-	
-	document.addEventListener('mousedown', onDocumentMouseDown, false);
-	document.addEventListener('touchstart', onDocumentTouchStart, false);
-	document.addEventListener('touchmove', onDocumentTouchMove, false);
-
-	
-	
-	function onDocumentMouseDown(event) {
-		event.preventDefault();
-		
-		document.addEventListener('mousemove', onDocumentMouseMove, false);
-		document.addEventListener('mouseup', onDocumentMouseUp, false);
-		document.addEventListener('mouseout', onDocumentMouseOut, false);
-		
-		mouseXOnMouseDown = event.clientX - windowHalfX;
-		targetRotationOnMouseDown = targetRotation;
-	}
-	function onDocumentMouseMove(event) {
-		mouseX = event.clientX - windowHalfX;
-		targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown)*0.02;
-	}
-	function onDocumentMouseUp(event) {
-		document.addEventListener('mousemove', onDocumentMouseMove, false);
-		document.addEventListener('mouseup', onDocumentMouseUp, false);
-		document.addEventListener('mouseout', onDocumentMouseOut, false);
-	}
-	function onDocumentMouseDown(event) {
-		document.addEventListener('mousemove', onDocumentMouseMove, false);
-		document.addEventListener('mouseup', onDocumentMouseUp, false);
-		document.addEventListener('mouseout', onDocumentMouseOut, false);
-	}
-	function onDocumentTouchStart(event) {
-		if (event.touches.length ==1) {
-			event.preventDefault();
-			
-			mouseXOnMouseDown = event.touches[0].pageX-windowHalfX;
-			targetRotationOnMouseDown = targetRotation;
-		}
-	}
-	function onDocumentTouchMove(event) {
-		if (event.touches.length == 1 ) {
-			event.preventDefault();
-			
-			mouseX = event.touches[0].pageX-windowHalfX;
-			targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown)*0.05;
-		}
-	}
 };
 titleMesh = new THREE.Mesh(geometry, materials);
 titleMesh.position.x = centerOffSet;
 titleMesh.position.y = 100;
 titleMesh.position.z = 0;
 	
-titleMesh.rotation.x = 0;
-titleMesh.rotation.y = Math.PI*2;
 	
 group = new THREE.Group();
 group.add(titleMesh);
