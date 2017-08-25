f948d3308d0c2bf7253bdac6cb762cdc6ec315a4
ECallanan <erincallanan1996@gmail.com>
2017-08-25 14:15:30 +0100

    Update tweenFunctions.js

--- tween/tweenFunctions.js
+++ tween/tweenFunctions.js
@@ -2,7 +2,7 @@
 
 var a, simElectron, molecule, human, earth, solarsystem, stars, galaxy, GAMA, CMB;
 
-var Title, tweenForward0, tweenForward1, tweenForward2, tweenForward3, tweenForward4, tweenForward5, tweenForward6, tweenForward7
+var tweenForward0, tweenForward1, tweenForward2, tweenForward3, tweenForward4, tweenForward5, tweenForward6, tweenForward7
 var tweenForward8, tweenForward9, tweenForward10, tweenForward11, tweenForward12, tweenForward13, tweenForward14
 
 function initTweens(){
