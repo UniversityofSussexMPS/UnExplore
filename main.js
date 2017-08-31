762c5a71ed0f44b1e9b235f5223a207829880de7
ECallanan <erincallanan1996@gmail.com>
2017-08-31 19:23:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -119,8 +119,11 @@ function init() {
 	loadScript("data/GAMA_data.js",addGamaData)
 
 	//Add the first model to the scene
-	scene.add(titleMesh);
+	scene.add(proton1, proton2, neutron1, neutron2);
+	scene.add(particleSystem);
 	scene.add(light, ambientLight)
+	simElectron = true;
+	message.innerHTML="This is a Helium nucleus surrounded by a cloud of electrons";
 
 	//Initalize all the tweens
 	initTweens();
