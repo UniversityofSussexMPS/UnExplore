aef4fc43fd1e47ebb7e51342bd9fdb506bcb8348
ECallanan <erincallanan1996@gmail.com>
2017-09-05 16:33:38 +0100

    Update and rename title.html to title.js

---
+++ models/title.js
@@ -0,0 +1,7 @@
+var material = new THREE.MeshPhongMaterial({		
+	color: 0x8c1717		
+});		
+var textGeom = new THREE.TextGeometry('unExplore', {		
+	font: 'abscissa_bold'		
+});		
+var titleMesh = new THREE.Mesh(textGeom, material);
