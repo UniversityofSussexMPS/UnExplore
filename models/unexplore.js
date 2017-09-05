955aefaccb13e03cd92c00ae839b256d6fbee20f
ECallanan <erincallanan1996@gmail.com>
2017-09-05 14:51:08 +0100

    Update and rename unexplore.js to title.html

--- models/unexplore.js
+++
@@ -1,7 +0,0 @@
-var material = new THREE.MeshPhongMaterial({
-	color: 0x8c1717
-});
-var textGeom = new THREE.TextGeometry('unExplore', {
-	font: 'abscissa_bold'
-});
-var titleMesh = new THREE.Mesh(textGeom, material);
